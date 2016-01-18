233eb264380e290d1ab0d13a9cd085bc5dc4208f
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-01-18 17:09:20 +0530

    Add pythagorean triples code

---
+++ assignment_1/right_triangles/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int a;
+	int b;
+	int c;
+	int n = 1000;
+
+	for (a = 1; a < n; a++) {
+		for (b = 1; b < a; b++) {
+			for (c = 1; c < b; c++) {
+				if ((a * a) == ((b * b) + (c * c))) {
+					printf("%d %d %d\n", a, b, c);
+				}
+			}
+		}
+	}
+
+	return 0;
+}
